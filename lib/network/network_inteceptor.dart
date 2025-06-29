b2a16fc6781d041beee1232a261b728f4960a5e4
VoidElle <lucadelc@gmail.com>
2025-06-30 00:38:01 +0200

    feat: Enhance network interceptor to show and hide loading dialog during requests

--- lib/network/network_inteceptor.dart
+++ lib/network/network_inteceptor.dart
@@ -1,22 +1,49 @@
 import 'package:dio/dio.dart';
-
-// Todo: Loaders
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:open_pico_app/utils/global_singleton.dart';
+import 'package:open_pico_app/widgets/dialogs/loading_dialog.dart';
 
 class NetworkInterceptor extends InterceptorsWrapper {
 
   @override
-  void onRequest(RequestOptions options, RequestInterceptorHandler handler) {
-    super.onRequest(options, handler);
+  Future<dynamic> onRequest(RequestOptions options, RequestInterceptorHandler handler) async {
+
+    // Retrieve the current context from the navigator key
+    final BuildContext context = GlobalSingleton.navigatorKey.currentState!.context;
+
+    // Show the loading dialog
+    showDialog<LoadingDialog>(
+      context: context,
+      useSafeArea: false,
+      builder: (BuildContext context) => const LoadingDialog(),
+    );
+
+    return super.onRequest(options, handler);
   }
 
   @override
-  void onResponse(Response response, ResponseInterceptorHandler handler) {
-    super.onResponse(response, handler);
+  Future<dynamic> onResponse(Response<dynamic> response, ResponseInterceptorHandler handler) async {
+
+    // Retrieve the current context from the navigator key
+    final BuildContext context = GlobalSingleton.navigatorKey.currentState!.context;
+
+    // Hide the loading dialog
+    context.pop();
+
+    return super.onResponse(response, handler);
   }
 
   @override
-  void onError(DioException err, ErrorInterceptorHandler handler) {
-    super.onError(err, handler);
+  Future<dynamic> onError(DioException err, ErrorInterceptorHandler handler) async {
+
+    // Retrieve the current context from the navigator key
+    final BuildContext context = GlobalSingleton.navigatorKey.currentState!.context;
+
+    // Hide the loading dialog
+    context.pop();
+
+    return super.onError(err, handler);
   }
 
 }
