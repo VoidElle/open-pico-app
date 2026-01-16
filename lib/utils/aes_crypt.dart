7ccaa466b5debb6f624e05937944ebd2a032a6f3
VoidElle <lucadelc@gmail.com>
2026-01-16 15:53:17 +0100

    fix: Missing utils folder in build.yaml

--- lib/utils/aes_crypt.dart
+++ lib/utils/aes_crypt.dart
@@ -7,7 +7,7 @@ import 'package:open_pico_app/repositories/global_token_repository.dart';
 import 'package:open_pico_app/utils/constants/cypher_constants.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 
-part 'aes_crypt.g.dart';
+part 'generated/aes_crypt.g.dart';
 
 class AESCrypt {
 
